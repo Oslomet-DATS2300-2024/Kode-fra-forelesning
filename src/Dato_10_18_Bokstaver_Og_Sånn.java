1544b4cecfd2ca15169d864b56a310a5768a5435
Nikolai Bjørnestøl Hansen <nikobjo@oslomet.no>
2024-10-18 14:35:14 +0200

    Laget main-metode som tester ut bokstaver-til-tall og omvendt.

---
+++ src/Dato_10_18_Bokstaver_Og_Sånn.java
@@ -0,0 +1,14 @@
+
+public class Dato_10_18_Bokstaver_Og_Sånn {
+    public static void main(String[] args) {
+        System.out.println((int) 'A'); // 65
+        System.out.println((int) 'B'); // 66
+        System.out.println((int) 'Z'); // 90
+        System.out.println((char) 91); // '['
+        System.out.println((char) 92); // '\'
+        System.out.println((int) ']'); // 93
+        System.out.println((int) 'a'); // 97
+        System.out.println((int) 'z'); // 122
+        System.out.println((int) 'Å'); // 197
+    }
+}
